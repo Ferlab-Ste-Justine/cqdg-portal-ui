33115c0ec38bee9db63bc04d568b509afe2b432f
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2025-04-16 09:03:15 -0400

    feat: CQDG-772 update variant save set trad (#350)

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -565,6 +565,7 @@ const en = {
       success: {
         titleCreate: 'Your set has been saved.',
         messageCreate: 'You can add your sets to a query from the sidebar or the dashboard.',
+        messageCreateVariant: 'You can add your sets to a query from the dashboard.',
         titleUpdate: 'Success',
         messageUpdate: 'Your set has been updated.',
         titleDelete: 'Success',
