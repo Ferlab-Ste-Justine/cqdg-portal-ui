4a51b850d4d988ae25be079e6e4de2577fdd50cb
kstonge <karine.st-onge.hsj@ssss.gouv.qc.ca>
2026-05-22 06:23:52 -0400

    feat: CQDG-1447 Adjust Cypress tests to fix issues

--- cypress/e2e/Telechargement/SampleDataTableauBiospecimens.cy.ts
+++ cypress/e2e/Telechargement/SampleDataTableauBiospecimens.cy.ts
@@ -10,7 +10,7 @@ describe('Page Data Exploration (Biospecimens) - Télécharger le sample data', 
 
     cy.login();
     cy.visitDataExploration('biospecimens', '?sharedFilterId=27aa6ea1-1ace-4661-b716-db987b0f78fb');
-    cy.get('div[role="tabpanel"] [class*="ant-table-row"]').eq(0).find('[type="checkbox"]').check({force: true});
+    cy.get('div[role="tabpanel"] [class*="ant-table-row"]:contains("PT0000817")').find('[type="checkbox"]').check({force: true});
     cy.clickAndIntercept('[class*="Header_ProTableHeader"] button[class*="ant-btn-default"]', 'POST', '**/file-manifest', 1, 1);
     cy.waitUntilFile(oneMinute);
   };
